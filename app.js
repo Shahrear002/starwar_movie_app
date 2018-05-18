9e5ef7342838785c938e8e02a33045c6dbe5ee75
Shahrear002 <shabuz2900@gmail.com>
2018-05-19 00:26:37 +0600

    Final work for production

--- app.js
+++ app.js
@@ -22,6 +22,4 @@ app.get('/star_wars_episode/:episode_number?', routes.movie_single);
 app.get('*', routes.notFound);
 
 
-app.listen(3000, function(){
-	console.log("This appliction is running on localhost:3000");
-});
+app.listen(process.env.PORT || 3000);
