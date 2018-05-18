424906e03f793319bb4464de8aa2cea5c2dc9b6c
Shahrear002 <shabuz2900@gmail.com>
2018-05-19 00:21:23 +0600

    Final Work Except Production

--- routes/index.js
+++ routes/index.js
@@ -13,11 +13,42 @@ exports.home = function(req,res){
 
 //movie_single
 exports.movie_single = function(req,res){
+
 	var episode_number = req.params.episode_number;
-	res.send("This is the page for episode " + episode_number);
+
+	var movies = moviesJSON.movies;
+
+	if (episode_number >= 1 && episode_number <= 6) {
+
+		var movie = movies[episode_number - 1];
+
+		var title = movie.title;
+
+		var main_characters = movie.main_characters;
+
+		res.render('movie_single', {
+			movies : movies,
+			title : title,
+			movie : movie,
+			main_characters: main_characters
+		});
+	} else {
+		res.render('notFound', {
+			movies : movies,
+			title : "This is not the page you are looking for"
+		});
+	}
+
+	
 };
 
 //notFound
 exports.notFound = function(req,res){
-	res.send("This is not the page that you are looking for");
+
+	var movies = moviesJSON.movies;
+
+	res.render('notFound', {
+		movies : movies,
+		title : "This is not the page you are looking for"
+	});
 };
